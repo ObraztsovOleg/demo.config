870bb6dd201a4ba7ab87f92eb1b6f80ea8d717c8
Obraztsov O.A. <olegrasputina@gmail.com>
2026-07-20 11:02:29 +0300

    Use Maven layout Nexus cache filenames

--- generateSBOMCyclonedxNexus.groovy
+++ generateSBOMCyclonedxNexus.groovy
@@ -711,7 +711,8 @@ def prepareCacheKey(def entry, def cdxgenVersion, def mavenSettingsId) {
 
 def getCacheObjectUrl(def cacheConfig, def entry) {
     if (!cacheConfig || !entry.cacheKey) return null
-    return "${cacheConfig.url}/${cacheConfig.prefix}/${entry.cacheKey}.json"
+    def cacheFilePrefix = cacheConfig.prefix.toString().tokenize("/").join("-")
+    return "${cacheConfig.url}/${cacheConfig.prefix}/${cacheFilePrefix}-${entry.cacheKey}.json"
 }
 
 def prepareCacheContext(def globals, def cacheConfig) {
